34b19bdc4642ecac437ad22f210cd29e8cbf764d
Dong Hun Yi <dhyi@Dongs-MacBook.local>
2017-09-08 00:56:10 -0400

    First Incomplete

---
+++ Project 3 - Do It/CreateTaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateTaskViewController.swift
+//  Project 3 - Do It
+//
+//  Created by Dong Hun Yi on 9/8/17.
+//  Copyright © 2017 PrinceYiCoding. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project 3 - Do It/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Project 3 - Do It
+//
+//  Created by Dong Hun Yi on 9/8/17.
+//  Copyright © 2017 PrinceYiCoding. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    
+    var important = false
+}
